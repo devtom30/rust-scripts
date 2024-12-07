3504ef84396692bb6a06ac2f4a8f83dd46bed01a
devtom30 <devtom30@gmail.com>
2024-12-08 00:52:07 +0100

    put each post in dir tree by date out/<YEAR>/<MONTH>/<DAY>

--- extract_blog_posts_from_export/src/main.rs
+++ extract_blog_posts_from_export/src/main.rs
@@ -1,11 +1,13 @@
 use std::char;
-use std::fs::File;
+use std::fs::{create_dir, exists, File};
 use std::fs::read_to_string;
 use std::io::{BufWriter, Write};
 
 use regex::Regex;
 
 fn main() {
+    let out_directory = "out";
+
     let str_to_convert = String::from("GIORNO 3 - gioved&#xEC; 5 febbraio : PISA !!!");
     let new_str = convert_special_chars_in_str(&str_to_convert);
     println!("{}", new_str);
@@ -24,9 +26,11 @@ fn main() {
     let mut post_open = false;
     let re_post_aperture = Regex::new(r"<post><title>(.*)</title>").unwrap();
     let re_post_closure = Regex::new(r"</post>").unwrap();
+    let re_post_aperture_publish_date = Regex::new(r"<created_at>(\d\d\d\d)-(\d\d)-(\d\d).*</created_at>").unwrap();
     let mut vec_current_post: Vec<String> = vec!();
     let mut current_post_number = 0;
     let mut current_post_title: String = String::from("");
+    let mut date_path = String::from("");
 
     for line in read_to_string(
         "/home/tom/.config/JetBrains/RustRover2024.1/scratches/scratch_2.xml")
@@ -40,7 +44,12 @@ fn main() {
             if re_post_closure.is_match(line) {
                 post_open = false;
                 let converted_title = convert_special_chars_in_str(&current_post_title);
-                write_vec_to_file(current_post_number, vec_current_post, &converted_title);
+                write_vec_to_file(
+                    current_post_number,
+                    vec_current_post,
+                    &converted_title,
+                    &date_path
+                );
                 vec_current_post = vec!();
             }
         } else {
@@ -49,6 +58,18 @@ fn main() {
                 current_post_number += 1;
                 current_post_title = String::from_utf8(Vec::from(caps.get(1).map_or("", |m| m.as_str()).as_bytes())).unwrap();
                 vec_current_post.push(line.parse().unwrap());
+
+                if let Some(caps) = re_post_aperture_publish_date.captures(line) {
+                    date_path = out_directory.to_string();
+                    date_path.push('/');
+                    date_path.push_str(caps.get(1).expect("can't extract year").as_str());
+                    date_path.push('/');
+                    date_path.push_str(caps.get(2).expect("can't extract month").as_str());
+                    date_path.push('/');
+                    date_path.push_str(caps.get(3).expect("can't extract day").as_str());
+                } else {
+                    date_path = out_directory.to_string();
+                }
             }
         }
     }
@@ -82,8 +103,9 @@ fn convert_special_chars_in_str(str: &String) -> String {
     str_ok.replace("/", "|")
 }
 
-fn write_vec_to_file(nb: i32, vector: Vec<String>, title: &String) {
-    let file_name = format!("out/post_{number:0>3}__{title}.html", number=nb, title=title);
+fn write_vec_to_file(nb: i32, vector: Vec<String>, title: &String, path: &String) {
+    create_path_if_needed(path);
+    let file_name = format!("{path}/post_{number:0>3}__{title}.html", path=path, number=nb, title=title);
     let file = File::create(&file_name).expect(format!("can't create file {}", file_name).as_str());
     let mut file = BufWriter::new(file);
 
@@ -99,6 +121,20 @@ fn write_vec_to_file(nb: i32, vector: Vec<String>, title: &String) {
     writeln!(file, "</html>").unwrap();
 
     file.flush().unwrap();
+}
+
+fn create_path_if_needed(path: &String) {
+    let mut current_dir = String::from("");
+    for dir in path.split('/') {
+        if current_dir.is_empty() {
+            current_dir = dir.to_string();
+        } else {
+            current_dir = current_dir + "/" + dir;
+        }
+        if !exists(&current_dir).unwrap() {
+            create_dir(&current_dir).expect("can't create dir {}");
+        }
+    };
 }
 
 fn convert_utf8_code_to_char(raw: &String) -> Option<char> {
